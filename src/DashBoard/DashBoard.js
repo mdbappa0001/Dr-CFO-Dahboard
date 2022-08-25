0c3966d8c13708ff4c469d6c1e54766726f94856
mdbappa0001 <abutanif366@gmail.com>
2022-08-25 19:29:57 +0600

    card added

--- src/DashBoard/DashBoard.js
+++ src/DashBoard/DashBoard.js
@@ -1,54 +1,152 @@
 import React from 'react';
-import {Link, Outlet} from 'react-router-dom';
-import { AiOutlineAppstore,AiFillFolderAdd, AiFillSignal,AiOutlineUserAdd,AiOutlineSlackSquare } from "react-icons/ai";
+import { Link, Outlet } from 'react-router-dom';
+import patient from "../Images/patient.png";
+import vector from "../Images/Vector.png";
+import logo from "../Images/logo.png";
+import doctor from "../Images/doctor.png";
+import bel from "../Images/bel.png";
+import rac1 from "../Images/Rac-1.png";
+import rac2 from "../Images/Rac-2.png";
+import rac3 from "../Images/Rac-3.png";
+import './Dashboard.css';
+import { AiFillFolderAdd, AiFillSignal, AiOutlineUserAdd, AiOutlineSlackSquare, AiOutlineSearch } from "react-icons/ai";
 
 const DashBoard = () => {
     return (
         <div class="drawer drawer-mobile">
-        <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
-        <div class="drawer-content flex flex-col items-center justify-center">
-            <h2 className='text-4xl text-purple-500'>My DashBoard</h2>
-         <Outlet></Outlet>
-          <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden">Open drawer</label>
-        
-        </div> 
-        <div class="drawer-side">
-          <label for="my-drawer-2" class="drawer-overlay"></label> 
-          <ul class="menu p-4 overflow-y-auto w-[320px] bg-base-200 text-base-content">
-          
-            <li><Link to="/">
-            <span className='text-[27px] text-primary'><AiOutlineAppstore /></span>
-            <span className='text-[20px] text-primary' >Patient Management</span>
-            </Link></li>
+            <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+            <div class="drawer-content flex flex-col bg-base-200">
 
-            <li><Link to="/consulation">
-            <span className='text-[27px]'><AiFillFolderAdd /></span>
-            <span className='text-[20px]' >Online consultation</span>
-                </Link></li>
+                {/* <h2 className='text-4xl text-purple-500'>My DashBoard</h2> */}
 
-            <li><Link to="/consulation">
-            <span className='text-[27px]'><AiFillFolderAdd /> </span>
-            <span className='text-[20px]' >Clinic</span>
-                </Link></li>
+                <div className='container'>
+                    <div class="card w-[750px] h-[48px]  shadow-sm ml-8">
+                        <input type="text" placeholder="Search patient name, date" class="input input-bordered w-[960px]" />
+                    </div>
 
-            <li><Link to="/consulation">
-            <span className='text-[27px]'><AiOutlineUserAdd /></span>
-            <span className='text-[20px]' >Patients</span>
-                </Link></li>
+                    <div className='images'>
+                        <img className='bel' src={bel} alt="" />
+                        <img className='doctor' src={doctor} alt="" />
+                    </div>
+                </div>
 
-            <li><Link to="/consulation">
-            <span className='text-[27px]'><AiFillSignal /></span>
-            <span className='text-[20px] ' >Analytics</span>
-                </Link></li>
 
-            <li><Link to="/consulation">
-            <span className='text-[27px]'><AiOutlineSlackSquare /></span>
-            <span className='text-[20px]' >Legal </span>
-                </Link></li>
-          </ul>
-        
+
+                <div class="w-[1150px] h-[70px] bg-base-300 shadow-sm ml-8 mt-4 items-center">
+                    <div className='container-2'>
+                    <div className='ml-12'>
+                        <p className=' font-bold'>Add Your Bank & Activate Video Consultation</p>
+                        <p>Provide secure video consultation to your patients from the comfort of your home</p>
+                    </div>
+                    
+                    <div className='ml-56'>
+                    <input type="button" class="button" value="Get Started" />
+                    </div>
+                    </div>
+                </div>
+
+
+                <div className="grid grid-cols-4 gap-4 w-[1160px] ml-6 mt-4">
+
+                
+                <div className="carousel-item relative w-full">
+                    <div className='w-full h-full  top-0 left-0 absolute opacity-70'></div>
+                    <div className='w-full h-full flex flex-col absolute top-0 left-0  items-center justify-center'>
+                        <div className='py-5 text-white pl-6'>
+                            <p>No. of patients</p>
+                            <p className='text-xl font-bold'>224</p>
+                             </div>
+                    </div>
+                    <img src={rac1} className="w-full" alt='' />
+                </div>
+
+                <div className="carousel-item relative w-full">
+                    <div className='w-full h-full  top-0 left-0 absolute opacity-70'></div>
+                    <div className='w-full h-full flex flex-col absolute top-0 left-0  items-center justify-center'>
+                        <div className='py-5 text-white pl-6'>
+                            <p>Today’s Revenue</p>
+                            <p className='text-xl font-bold'>₹ 224</p>
+                             </div>
+                    </div>
+                    <img src={rac2} className="w-full" alt='' />
+                </div>
+
+                <div className="carousel-item relative w-full">
+                    <div className='w-full h-full  top-0 left-0 absolute opacity-70'></div>
+                    <div className='w-full h-full flex flex-col absolute top-0 left-0  items-center justify-center'>
+                        <div className='py-5 text-white pl-6'>
+                            <p>Monthly Revenue</p>
+                            <p className='text-xl font-bold'>₹ 224</p>
+                             </div>
+                    </div>
+                    <img src={rac3} className="w-full" alt='' />
+                </div>
+
+                <div className="carousel-item relative w-full">
+                    <div className='w-full h-full  top-0 left-0 absolute opacity-70'></div>
+                    <div className='w-full h-full flex flex-col absolute top-0 left-0  items-center justify-center'>
+                        <div className='py-5 text-white pl-6'>
+                            <p>Today’s No-show</p>
+                            <p className='text-xl font-bold'>3</p>
+                             </div>
+                    </div>
+                    <img src={rac2} className="w-full" alt='' />
+                </div>
+
+               
+                </div>
+
+
+
+
+
+                <Outlet></Outlet>
+                <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+
+            </div>
+            <div class="drawer-side">
+                <label for="my-drawer-2" class="drawer-overlay"></label>
+                <ul class="menu p-4 overflow-y-auto w-[320px] text-base-content">
+
+                    <div>
+                        <img className='vector' src={vector} alt="" />
+                        <img className='logo' src={logo} alt="" />
+                        <p className='title'>DRCFO</p>
+                    </div>
+
+                    <li><Link to="/">
+                        <img className='w-[27px] mt-4' src={patient} alt="" />
+                        <span className='text-[20px] mt-4 font-inter text-primary' >Patient Management</span>
+                    </Link></li>
+
+                    <li><Link to="/consulation">
+                        <span className='text-[27px]'><AiFillFolderAdd /></span>
+                        <span className='text-[20px] font-inter' >Online consultation</span>
+                    </Link></li>
+
+                    <li><Link to="/consulation">
+                        <span className='text-[27px]'><AiFillFolderAdd /> </span>
+                        <span className='text-[20px] font-inter' >Clinic</span>
+                    </Link></li>
+
+                    <li><Link to="/consulation">
+                        <span className='text-[27px]'><AiOutlineUserAdd /></span>
+                        <span className='text-[20px]' >Patients</span>
+                    </Link></li>
+
+                    <li><Link to="/consulation">
+                        <span className='text-[27px]'><AiFillSignal /></span>
+                        <span className='text-[20px] ' >Analytics</span>
+                    </Link></li>
+
+                    <li><Link to="/consulation">
+                        <span className='text-[27px]'><AiOutlineSlackSquare /></span>
+                        <span className='text-[20px]' >Legal </span>
+                    </Link></li>
+                </ul>
+
+            </div>
         </div>
-      </div>
     );
 };
 
--- src/DashBoard/PatientManagement.js
+++ src/DashBoard/PatientManagement.js
@@ -1,10 +1,216 @@
 import React from 'react';
+import './PatientManagement.css'
+import { AiOutlineRight, AiOutlineCheck } from "react-icons/ai";
 
 const PatientManagement = () => {
     return (
-        <div>
-            <h2>this is pathiednt</h2>
+        <div className=" w-[1160px] ml-6 mt-8">
+
+
+            <div className='container-3'>
+
+
+                <div class="card w-[440px] bg-base-100 shadow-xl">
+                    <div className='grid grid-cols-2 mt-4 mb-6'>
+                        <p className='ml-6 text-xl'>Requests (46)</p>
+                        <p className='ml-32 text-primary flex'>View All <span className='mt-1 ml-2'><AiOutlineRight /></span></p>
+                    </div>
+
+                    <div className='flex mt-6'>
+                        <div className='ml-6 mr-6'>
+                        <input type="button" class="button3" value="AM" />
+                        </div>
+                        <div>
+                            <p>Akash Mandal</p>
+                            <p className='text-xs'>10:30-11:00 AM</p>
+                        </div>
+                        <div className='ml-36'>
+                            <label class="btn btn-sm btn-active btn-ghost">✕</label>
+                            <label class="btn btn-sm btn-active btn-primary ml-4"><AiOutlineCheck /></label>
+                        </div>
+                    </div>
+
+                    <div className='flex mt-6'>
+                        <div className='ml-6 mr-6'>
+                        <input type="button" class="button3" value="AM" />
+                        </div>
+                        <div>
+                            <p>Akash Mandal</p>
+                            <p className='text-xs'>10:30-11:00 AM</p>
+                        </div>
+                        <div className='ml-36'>
+                            <label class="btn btn-sm btn-active btn-ghost">✕</label>
+                            <label class="btn btn-sm btn-active btn-primary ml-4"><AiOutlineCheck /></label>
+                        </div>
+                    </div>
+                    <div className='flex mt-6'>
+                        <div className='ml-6 mr-6'>
+                        <input type="button" class="button3" value="AM" />
+                        </div>
+                        <div>
+                            <p>Akash Mandal</p>
+                            <p className='text-xs'>10:30-11:00 AM</p>
+                        </div>
+                        <div className='ml-36'>
+                            <label class="btn btn-sm btn-active btn-ghost">✕</label>
+                            <label class="btn btn-sm btn-active btn-primary ml-4"><AiOutlineCheck /></label>
+                        </div>
+                    </div>
+                    <div className='flex mt-6'>
+                        <div className='ml-6 mr-6'>
+                        <input type="button" class="button3" value="AM" />
+                        </div>
+                        <div>
+                            <p>Akash Mandal</p>
+                            <p className='text-xs'>10:30-11:00 AM</p>
+                        </div>
+                        <div className='ml-36'>
+                            <label class="btn btn-sm btn-active btn-ghost">✕</label>
+                            <label class="btn btn-sm btn-active btn-primary ml-4"><AiOutlineCheck /></label>
+                        </div>
+                    </div>
+                    <div className='flex mt-6'>
+                        <div className='ml-6 mr-6'>
+                        <input type="button" class="button3" value="AM" />
+                        </div>
+                        <div>
+                            <p>Akash Mandal</p>
+                            <p className='text-xs'>10:30-11:00 AM</p>
+                        </div>
+                        <div className='ml-36'>
+                            <label class="btn btn-sm btn-active btn-ghost">✕</label>
+                            <label class="btn btn-sm btn-active btn-primary ml-4"><AiOutlineCheck /></label>
+                        </div>
+                    </div>
+
+                </div>
+
+
+
+                <div class="card w-[700px] bg-base-100 shadow-xl ml-6">
+                    <div className='grid grid-cols-2 mt-4'>
+                        <p className='ml-8 text-xl'>Today Appointments (16)</p>
+                        <p className='ml-60 text-primary flex'>View All <span className='mt-1 ml-2'><AiOutlineRight /></span></p>
+                    </div>
+
+                    <div className='flex mt-12'>
+                        <div className='ml-8 mr-4'>
+                            <input type="button" class="button3" value="AM" />
+                        </div>
+                        <div className='mr-12'>
+                            <p>Akash Mandal</p>
+                            <p className='text-xs'>10:30-11:00 AM</p>
+                        </div>
+                        <div className='mr-4 mt-2'>
+                            <p>10:30 AM</p>
+                        </div>
+                        <div className='mr-4 mt-2'>
+                            <input type="button" class="button1" value="Walk In" />
+                        </div>
+                        <div className='mr-12 mt-2'>
+                            <input type="button" class="button1" value="Walk In" />
+                        </div>
+                        <div>
+                            <input type="button" class="button2" value="Diagnose" />
+                        </div>
+                    </div>
+
+                    <div className='flex mt-6'>
+                        <div className='ml-8 mr-4'>
+                            <input type="button" class="button3" value="AM" />
+                        </div>
+                        <div className='mr-12'>
+                            <p>Akash Mandal</p>
+                            <p className='text-xs'>10:30-11:00 AM</p>
+                        </div>
+                        <div className='mr-4 mt-2'>
+                            <p>10:30 AM</p>
+                        </div>
+                        <div className='mr-4 mt-2'>
+                            <input type="button" class="button1" value="Walk In" />
+                        </div>
+                        <div className='mr-12 mt-2'>
+                            <input type="button" class="button1" value="Walk In" />
+                        </div>
+                        <div>
+                            <input type="button" class="button2" value="Diagnose" />
+                        </div>
+                    </div>
+
+                    <div className='flex mt-6'>
+                        <div className='ml-8 mr-4'>
+                            <input type="button" class="button3" value="AM" />
+                        </div>
+                        <div className='mr-12'>
+                            <p>Akash Mandal</p>
+                            <p className='text-xs'>10:30-11:00 AM</p>
+                        </div>
+                        <div className='mr-4 mt-2'>
+                            <p>10:30 AM</p>
+                        </div>
+                        <div className='mr-4 mt-2'>
+                            <input type="button" class="button1" value="Walk In" />
+                        </div>
+                        <div className='mr-12 mt-2'>
+                            <input type="button" class="button1" value="Walk In" />
+                        </div>
+                        <div>
+                            <input type="button" class="button2" value="Diagnose" />
+                        </div>
+                    </div>
+
+                    <div className='flex mt-6'>
+                        <div className='ml-8 mr-4'>
+                            <input type="button" class="button3" value="AM" />
+                        </div>
+                        <div className='mr-12'>
+                            <p>Akash Mandal</p>
+                            <p className='text-xs'>10:30-11:00 AM</p>
+                        </div>
+                        <div className='mr-4 mt-2'>
+                            <p>10:30 AM</p>
+                        </div>
+                        <div className='mr-4 mt-2'>
+                            <input type="button" class="button1" value="Walk In" />
+                        </div>
+                        <div className='mr-12 mt-2'>
+                            <input type="button" class="button1" value="Walk In" />
+                        </div>
+                        <div>
+                            <input type="button" class="button2" value="Diagnose" />
+                        </div>
+                    </div>
+
+                    <div className='flex mt-6'>
+                        <div className='ml-8 mr-4'>
+                            <input type="button" class="button3" value="AM" />
+                        </div>
+                        <div className='mr-12'>
+                            <p>Akash Mandal</p>
+                            <p className='text-xs'>10:30-11:00 AM</p>
+                        </div>
+                        <div className='mr-4 mt-2'>
+                            <p>10:30 AM</p>
+                        </div>
+                        <div className='mr-4 mt-2'>
+                            <input type="button" class="button1" value="Walk In" />
+                        </div>
+                        <div className='mr-12 mt-2'>
+                            <input type="button" class="button1" value="Walk In" />
+                        </div>
+                        <div>
+                            <input type="button" class="button2" value="Diagnose" />
+                        </div>
+                    </div>
+
+
+                </div>
+
+
+
+            </div>
         </div>
+
     );
 };
 
